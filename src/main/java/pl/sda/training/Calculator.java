82b06d12fb1ec6759cd9ee57d9f1759b6efe1511
sdakf <sdakftraining@gmail.com>
2018-08-25 13:42:26 +0200

    page for calculator

---
+++ src/main/java/pl/sda/training/Calculator.java
@@ -0,0 +1,16 @@
+package pl.sda.training;
+
+public class Calculator {
+    public static int adding(int x, int y) {
+        int result = x + y;
+        return result;
+    }
+    public static int subtracting(int x, int y) {
+        int result = x - y;
+        return result;
+    }
+    public static int multiplying(int x, int y) {
+        int result = x * y;
+        return result;
+    }
+}
--- src/main/java/pl/sda/training/Main.java
+++ src/main/java/pl/sda/training/Main.java
@@ -12,17 +12,31 @@ public class Main {
     public static void main(String[] args) {//metoda
         System.out.println("hello world");
         System.out.println(animals);
-        for(String text:animals){
+        for (String text : animals) {
             System.out.println(text.trim());
         }
         animals.stream()
-                .map(e->e.trim())
-                .forEach(e->System.out.println(e));
+                .map(e -> e.trim())
+                .forEach(e -> System.out.println(e));
 
         String animalsByComma = "";
-        for(String animal:animals){
-            animalsByComma=animalsByComma+animal+",";
+        int counter = 0;
+        for (String animal : animals) {
+            animalsByComma = animalsByComma + animal.trim();
+            counter = counter + 1;
+            if (counter < animals.size()) {
+                animalsByComma = animalsByComma + ",";
+            }
+            else {
+                animalsByComma = animalsByComma + ".";
+                            }
         }
         System.out.println(animalsByComma);
+        int sum = Calculator.adding(3,8);
+        System.out.println(sum);
+        int subtract = Calculator.subtracting(3,8);
+        System.out.println(subtract);
+        int multiply = Calculator.multiplying(3,8);
+        System.out.println(multiply);
     }
 }
